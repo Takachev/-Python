22040e6f00731c685415a5d536fe54338aa1798a
TDA_geekbrains <tda.nso@mail.ru>
2021-06-08 02:12:49 +0700

    Здравствуте. Сумел разобрать и перепечатать только 2-а первых здания. Задание с шутками, быстро разобрать не получается, его отправляю "бездумно".

---
+++ danil_takachev_dz_3_2.py
@@ -0,0 +1,14 @@
+name_list = ('Иван', 'Мария', 'Петр', 'Илья')
+
+def thesaurus(*names):
+    out_dict = dict()
+    for name in names:
+        out_dict.setdefault(name[0], [])
+        out_dict[name[0]].append(name)
+    return out_dict
+
+
+print(thesaurus("Иван", "Мария", "Петр", "Илья"))
+
+
+
